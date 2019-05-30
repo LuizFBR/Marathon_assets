9b910d32f98f1f11dae1f7d8e73f057d53d12b81
LuizFBR <38136395+LuizFBR@users.noreply.github.com>
2019-05-29 21:40:03 -0300

    Legibility

--- Shell_Scripts/create_cpp.sh
+++ Shell_Scripts/create_cpp.sh
@@ -15,7 +15,14 @@ read FILE_NAME
 mkdir "$DIR_NAME"
 touch "$DIR_NAME/$FILE_NAME.cpp"
 #writing the new .cpp marathon file:
-echo "#include <iostream>" >> "$DIR_NAME/$FILE_NAME.cpp"
-echo -e "#include <bits/stdc++.h>\n" >> "$DIR_NAME/$FILE_NAME.cpp"
-echo -e "using namespace std;\n">> "$DIR_NAME/$FILE_NAME.cpp"
-echo -e "int main()\n{\n\t\n}" >> "$DIR_NAME/$FILE_NAME.cpp" #write "string" >> PATH
+echo "\
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+}"\
+>> "$DIR_NAME/$FILE_NAME.cpp" #write "string" >> PATH

5b13aceb2a151fe2a42d9bcef00086ca29df4689
LuizFBR <38136395+LuizFBR@users.noreply.github.com>
2019-06-05 00:03:24 -0300

    Create FakeNP.cpp

---
+++ Solved Problems/FakeNP/FakeNP.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+
+int main()
+{
+
+    int N,M;
+
+    cin >> N >> M;
+
+    if ( M - N >= 1)cout<<2<<endl;
+    else cout<< M << endl;
+
+    return 0;
+}

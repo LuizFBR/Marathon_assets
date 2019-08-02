4af6b34f2b97bc18f26770a37db65d95a2014d43
Luiz Fernando Bueno Rosa <luizfernando5687@gmail.com>
2019-08-02 14:12:14 -0300

    Update create_cpp.sh

--- Shell_Scripts/create_cpp.sh
+++ Shell_Scripts/create_cpp.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-echo "Please Input Directory Name"
 echo "Current directory:" #for quick reference
 pwd
 
+echo "Please Input Directory Name"
 read DIR_NAME
+
 while [ "${DIR_NAME: -1}" == "/" ] #string formatting;
 do
   DIR_NAME=${DIR_NAME%/}
 done
+
 echo "Input file name"
 read FILE_NAME
 
@@ -17,7 +19,6 @@ touch "$DIR_NAME/$FILE_NAME.cpp"
 #writing the new .cpp marathon file:
 echo "\
 #include <bits/stdc++.h>
-
 using namespace std;
 
 int main()
